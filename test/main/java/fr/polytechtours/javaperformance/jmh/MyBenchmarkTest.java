dc20f1cce45d0594ebc98bb3a44b13bf17cd33a6
painPainBoulanger <emeric.pain@gmail.com>
2019-09-24 09:45:29 +0200

    1st commit - init repo

---
+++ test/main/java/fr/polytechtours/javaperformance/jmh/MyBenchmarkTest.java
@@ -0,0 +1,12 @@
+package fr.polytechtours.javaperformance.jmh;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+class MyBenchmarkTest {
+
+    @Test
+    void testMethod() {
+    }
+}
